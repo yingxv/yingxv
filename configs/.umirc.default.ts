7c31013fb51f886edecc2fe63d985dc7c6dbf3c2
fuRan <NgeKaworu@gmail.com>
2022-01-04 17:46:18 +0800

    Squashed 'src/js-sdk/' changes from ae2672e..3e79c3a
    3e79c3a feat:
    109fd8a opt: chunk
    
    git-subtree-dir: src/js-sdk
    git-subtree-split: 3e79c3ada7b6e0f64f1dc7709d0f59fe9301646e

--- configs/.umirc.default.ts
+++ configs/.umirc.default.ts
@@ -22,14 +22,8 @@ export default defineConfig({
   sass: false,
   hash: true,
   runtimePublicPath: true,
-  externals: {
-    moment: 'moment',
-    '@ant-design/icons': '@ant-design/icons',
-  },
-  scripts: [
-    'https://lib.baomitu.com/moment.js/latest/moment.min.js',
-    'https://cdnjs.cloudflare.com/ajax/libs/ant-design-icons/4.7.0/index.umd.min.js',
-  ],
+  externals: { moment: 'moment' },
+  scripts: ['https://lib.baomitu.com/moment.js/latest/moment.min.js'],
   analyze: {
     analyzerMode: 'server',
     analyzerPort: 8888,
@@ -40,33 +34,38 @@ export default defineConfig({
     logLevel: 'info',
     defaultSizes: 'gzip', // stat  // gzip
   },
-  chunks: ['react', 'umi', 'antd', 'lodash', 'vendors'],
-  chainWebpack: (config: any) => {
-    config.optimization.splitChunks({
-      cacheGroups: {
-        antd: {
-          name: 'antd',
-          chunks: 'all',
-          test: /[\\/]node_modules[\\/](antd|@ant-design|rc-.*)[\\/]/,
-          priority: 4,
-        },
-        react: {
-          name: 'react',
-          chunks: 'all',
-          test: /[\\/]node_modules[\\/](react|react-dom|react-router|react-router-dom)[\\/]/,
-          priority: 3,
-        },
-        lodash: {
-          name: 'lodash',
-          chunks: 'all',
-          test: /[\\/]node_modules[\\/](lodash)[\\/]/,
-          priority: 2,
-        },
-        vendors: {
-          name: 'vendors',
-          chunks: 'all',
-          test: /[\\/]node_modules[\\/]/,
-          priority: 1,
+  chunks: ['react', 'lodash', 'icons', 'antd', 'umi'],
+  chainWebpack: (config) => {
+    config.merge({
+      optimization: {
+        splitChunks: {
+          minChunks: 1,
+          cacheGroups: {
+            icons: {
+              name: 'icons',
+              chunks: 'all',
+              test: /[\\/]node_modules[\\/]@ant-design[\\/](icons|icons-svg)[\\/]/,
+              priority: 15,
+            },
+            antd: {
+              name: 'antd',
+              chunks: 'all',
+              test: /[\\/]node_modules[\\/](antd|rc-.*)[\\/]/,
+              priority: 14,
+            },
+            react: {
+              name: 'react',
+              chunks: 'all',
+              test: /[\\/]node_modules[\\/](react|react-dom|react-router|react-router-dom)[\\/]/,
+              priority: 13,
+            },
+            lodash: {
+              name: 'lodash',
+              chunks: 'all',
+              test: /[\\/]node_modules[\\/]lodash[\\/]/,
+              priority: 12,
+            },
+          },
         },
       },
     });
